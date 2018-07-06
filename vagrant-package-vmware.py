4b5692a424e4c476ca4bf3d145cb6f3f5ac0d25d
Dominik Schlecht <dominik.schlecht@hotmail.de>
2018-07-06 15:05:01 +0200

    Fixed tar-paths

--- vagrant-package-vmware.py
+++ vagrant-package-vmware.py
@@ -10,7 +10,7 @@ from os import system as run
 
 parser = argparse.ArgumentParser()
 parser.add_argument("vmx", help="path to the vmx-file")
-parser.add_argument("vmdk", help="path to the main vmdk")
+parser.add_argument("--vmdk", help="path to the main vmdk", required=False)
 parser.add_argument("name", help="box-name")
 parser.add_argument("version", help="version-identifier")
 parser.add_argument(
@@ -32,13 +32,36 @@ print("[+] Defragging and shrinking disk.")
 # run("vmware-vdiskmanager -d '{}'".format(args.vmdk))
 # run("vmware-vdiskmanager -k '{}'".format(args.vmdk))
 
-# Create metadata-file
+# Add all other files which should be packed into the box
+# See vagrant box documentation
+# https://www.vagrantup.com/docs/vmware/boxes.html
+
+# Open the tarfile
+print("[+] Creating .box-file - hang on, this might take some time.")
+box_file = tarfile.open(
+    "{}/{}_{}.box".format(args.dest_dir, args.name, args.version),
+    'w:gz',
+    compresslevel=args.compresslevel
+)
+
+path, filename = os.path.split(args.vmx)
+os.chdir(path)
+box_file.add(filename)
+
+vm_name = re.match("(.*)\.vmx", filename).group(1) # TODO Match to end
+box_file.add("{}.nvram".format(vm_name))
+box_file.add("{}.vmsd".format(vm_name))
+box_file.add("{}.vmxf".format(vm_name))
+
+# Create and add metadata-file
 metadata ="""{
   "provider": "vmware_workstation"
 }"""
-# metadata_file = open("metadata.json", 'w')
-# metadata_file.write(metadata)
-# metadata_file.close()
+metadata_file = open("metadata.json", 'w')
+metadata_file.write(metadata)
+metadata_file.close()
+box_file.add("metadata.json")
+
 
 # Build regex to find the currently used vmdk-files to pack
 # See specification from vmware http://www.vmware.com/app/vmdk/?src=vmdk
@@ -52,40 +75,28 @@ re_vmdk = re.compile(
     re_size + "\s" + 
     re_type + "\s" +
     re_name)
-    
-vmdk_list = re_vmdk.findall(open(args.vmdk).read())
 
-# Add all other files which should be packed into the box
-# See vagrant box documentation
-# https://www.vagrantup.com/docs/vmware/boxes.html
+# Add the VMDK itself
+print(args.vmdk)
+if args.vmdk == None:
+    main_vmdk = "{}.vmdk".format(vm_name)
+else:
+    path, filename = os.path.split(args.vmx)
+    os.chdir(path)
+    main_vmdk = filename
 
-# Open the tarfile
-print("[+] Creating .box-file - hang on, this might take some time.")
-box_file = tarfile.open(
-    "{}/{}_{}.box".format(args.dest_dir, args.name, args.version),
-    'w:gz',
-    compresslevel=args.compresslevel
-)
+print(main_vmdk)
+box_file.add(main_vmdk)
 
-# Get the path and append to sub-vmdks
-path, filename = os.path.split(args.vmdk)
+# Add all sub-files
+vmdk_list = re_vmdk.findall(open(main_vmdk).read())
 for vmdk_file in vmdk_list:
     print("[*] Adding vmdk-file {} from {}.".format(
             vmdk_list.index(vmdk_file)+1,
             len(vmdk_list)
         )
     )
-    box_file.add(path + "/" + vmdk_file)
-    
-print("[*] Adding other files, this should be fast.")
-box_file.add(args.vmdk)
-box_file.add(args.vmx)
+    box_file.add(vmdk_file)    
 
-vm_name = re.match("(.*)\.vmx", args.vmx).group(1)
-box_file.add("{}.nvram".format(vm_name))
-box_file.add("{}.vmsd".format(vm_name))
-box_file.add("{}.vmxf".format(vm_name))
-
-box_file.addfile(tarfile.TarInfo("metadata.json"), metadata)
-    
+# Close the tar-file
 box_file.close()
